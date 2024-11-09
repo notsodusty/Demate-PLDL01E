e5298b5fcea99af129cac0475a67367277845217
Carl Demate <2023-2-00945@lpunetwork.edu.ph>
2024-11-09 12:17:40 +0800

    9/11/24

---
+++ MidtermQ1.py
@@ -0,0 +1,69 @@
+class CustomerInfo: # Class for Customer Information
+    def __init__(self): # initialize the customer info
+        self.customer_name = input("Enter customer name: ")
+        self.house_number = input("Enter house number: ")
+        self.street = input("Enter street:")
+        self.barangay = input("Enter barangay: ")
+        self.municipality = input("Enter municipality: ")
+        self.city = input("Enter city: ")
+        self.customer_account_number = input("Enter customer account number: ")
+
+class ElectricBill: # Class for Electric Bill
+    def __init__(self):
+        self.balance = int(input("Enter balance from previous billing: "))
+        self.current_amount_due = input("Enter amount due: ")
+        self.current_due_date = input("Enter due date: ")
+        self.total_amount_due = 0
+
+    def get_total_amount_due(self):
+        self.total_amount_due = self.balance + self.current_amount_due
+        return self.total_amount_due
+
+
+class ServiceInfo:
+    def __init__(self, customer):
+        self.customer = customer
+        self.service_id_number = customer.customer_account_number
+        self.rate = input("Enter Residential or Business: ")
+        self.contact = customer.customer_name
+        self.contact_house_number = customer.house_number
+        self.contact_street = customer.street
+        self.contact_barangay = customer.barangay
+        self.contact_municipality = customer.municipality
+        self.contact_city = customer.city
+
+
+class BillingInfo:
+    def __init__(self, bill):
+        self.bill = bill
+        self.bill_date = input("Enter bill date: ")
+        self.meter_reading_date = input("Enter meter reading date: ")
+        self.bill_period = input("Enter bill period: ")
+        self.due_date = bill.current_due_date
+        self.total_kwh = int(input("Enter total KWH: "))
+        self.rate_per_kwh = int(input("Enter rate per KWH:"))
+        self.total_current_amount = bill.total_amount_due
+        self.next_meter_reading = input("Enter next meter reading: ")
+        self.amount_due = 0
+
+    def get_amount_due(self):
+        self.amount_due = self.rate_per_kwh * self.total_kwh
+        return self.amount_due
+
+
+class BillComputationSummary:
+    def __init__(self, bill):
+        self.billing = bill
+        self.remaining_balance = bill.balance
+        self.generation = input("Enter Generation fee: ")
+        self.transmission = input("Enter Transmission fee: ")
+        self.system_loss = input("Enter System loss: ")
+        self.distribution = input("Enter Distribution fee:")
+        self.subsidies = input("Enter subsidies fee: ")
+        self.government_taxes = input("Enter Government Taxes: ")
+        self.universal_charges = input("Enter Universal charges")
+        self.fit_all = input("Enter FiT-All (Renewable) fee: ")
+        self.applied_credits = input("Enter applied credits: ")
+        self.other_charges = input("Enter other charges: ")
+        self.installment_due = input("Enter installment due: ")
+
---
+++ Midtermdisplay.py
@@ -0,0 +1,87 @@
+import MidtermQ1
+
+def main():
+
+    customer = MidtermQ1.CustomerInfo()
+    electric_bill = MidtermQ1.ElectricBill()
+    service_info = MidtermQ1.ServiceInfo(customer)
+    billing_info = MidtermQ1.BillingInfo(electric_bill)
+    bill_summary = MidtermQ1.BillComputationSummary(electric_bill)
+
+    display_customer_info(customer)
+    display_electric_bill(electric_bill)
+    display_service_info(service_info)
+    display_billing_info(billing_info)
+    display_bill_summary(bill_summary)
+
+
+def display_customer_info(customer):
+    # display customer info
+    print(f'''
+    {customer.customer_name}
+    {customer.house_number}
+    {customer.street}
+    {customer.barangay}
+    {customer.municipality}
+    {customer.city}
+    {customer.customer_account_number}
+
+     **************************
+    ''')
+
+def display_electric_bill(electric_bill):
+
+    # Display electric bill info
+    if electric_bill.balance == 0:
+        print("Thank You")
+    else:
+        print("Please pay remaining balance.")
+
+    print(f'''
+    {electric_bill.current_amount_due}
+    {electric_bill.current_due_date}
+    {electric_bill.total_amount_due}
+
+
+    **************************
+    ''')
+def display_service_info(service_info):
+    print(f'''
+    Service ID Number: {service_info.service_id_number}
+    Rate: {service_info.rate}
+    Contact in the name of : {service_info.contact}
+    Service Address: {service_info.contact_house_number}
+    {service_info.contact_street} {service_info.contact_barangay}
+    {service_info.contact_city} {service_info.contact_municipality}
+    **************************
+    ''')
+
+def display_billing_info(billing_info):
+    print(f'''
+    Bill Date: {billing_info.bill_date}
+    Meter Reading Date: {billing_info.meter_reading_date}
+    Bill Period: {billing_info.bill_period}
+    Due Date: {billing_info.due_date}
+    Total KWH: {billing_info.total_kwh}
+    Total Current Amount: {billing_info.total_current_amount}
+    Next Meter Reading: {billing_info.next_meter_reading}
+    **************************
+    ''')
+
+def display_bill_summary(bill_summary):
+    print(f'''
+    Generation: {bill_summary.generation}
+    Transmission: {bill_summary.transmission}
+    System Loss: {bill_summary.system_loss}
+    Distribution (Meralco): {bill_summary.distribution}
+    Subsidies: {bill_summary.subsidies}
+    Government Taxes: {bill_summary.government_taxes}
+    Universal Charges: {bill_summary.universal_charges}1
+    FiT-All (Renewable): {bill_summary.fit_all}
+    Applied Credits: {bill_summary.applied_credits}
+    Other Charges: {bill_summary.other_charges}
+    Installment Dues: {bill_summary.installment_due}
+     **************************
+    ''')
+
+main()
